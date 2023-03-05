2941b8090223e9f584dbbfc9c09e108dbe0f19f1
tutti-hobbyist <tutti.hobbyist@gmail.com>
2023-03-06 08:54:28 +0900

    fin chap9

---
+++ private_diary/private_diary/settings_dev.py
@@ -0,0 +1,49 @@
+from .settings_common import *
+
+DEBUG = env('DEBUG')
+
+ALLOWED_HOSTS = []
+
+# 開発時のメール送信先をコンソールに指定
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# ロギング設定
+LOGGING = {
+    'version': 1,  # 1固定
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        # diaryアプリケーションが利用するロガー
+        'diary': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+
+    # ハンドラの設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'dev': {
+            'format': '\n'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
